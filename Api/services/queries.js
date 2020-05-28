9194d888209b86c25f0ab7a4f151e587359c48b6
Jair Balcazar <Jairbalcazar.jb@gmail.com>
2020-05-27 21:03:07 -0500

    Katarogu and Product queries add

--- Api/services/queries.js
+++ Api/services/queries.js
@@ -1,7 +1,18 @@
-const {MongoLib, ObjectId} = require('../lib/mongo');
- 
-const mongoDB = new MongoLib;
+const KatarogusService = require('../services/katarogus');
+const ProductService = require('../services/products');
+const katarogusService = new KatarogusService();
+const productService = new ProductService();
 
 module.exports = {
+  getKatarogu: async (root, { kataroguId }) => {
+    return await katarogusService.getKatarogu(kataroguId);
+  },
 
+  getProducts: async (root, { kataroguId }) => {
+    return await productService.getProducts({kataroguId});
+  },
+
+  getProduct: async (root, { productId }) => {
+    return await productService.getProduct(productId);
+  }
 };
